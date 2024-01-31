9dbfbe2e1cd927f56851f4e9f931b2232c66c6a4
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-01-31 20:48:35 +0000

    refactor: rename github env

--- packages/auth/src/env.js
+++ packages/auth/src/env.js
@@ -6,13 +6,13 @@ export const env = createEnv({
   extends: [vercel],
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']).optional(),
-    GITHUB_CLIENT_ID: z.string(),
-    GITHUB_CLIENT_SECRET: z.string(),
+    AUTH_GITHUB_CLIENT_ID: z.string(),
+    AUTH_GITHUB_CLIENT_SECRET: z.string(),
   },
   runtimeEnv: {
     NODE_ENV: process.env['NODE_ENV'],
-    GITHUB_CLIENT_ID: process.env['GITHUB_CLIENT_ID'],
-    GITHUB_CLIENT_SECRET: process.env['GITHUB_CLIENT_SECRET'],
+    AUTH_GITHUB_CLIENT_ID: process.env['GITHUB_CLIENT_ID'],
+    AUTH_GITHUB_CLIENT_SECRET: process.env['GITHUB_CLIENT_SECRET'],
   },
   emptyStringAsUndefined: true,
 });
