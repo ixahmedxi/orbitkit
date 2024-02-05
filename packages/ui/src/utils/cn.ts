6501c44a0b57735fca4705485c1528d3e2901cc9
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-02-05 18:11:44 +0000

    feat(marketing): light/dark mode

--- packages/ui/src/utils/cn.ts
+++ packages/ui/src/utils/cn.ts
@@ -1,8 +1,11 @@
 import type { ClassValue } from 'clsx';
 
 import { clsx } from 'clsx';
+import { createTwc } from 'react-twc';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const twx = createTwc({ compose: cn });
