9c54d72e8a9c21f941ff6fe7980d61785a4ddd56
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-03-12 05:10:26 +0000

    fix: remove pnpm scopes commitlint package

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,5 @@
 const config = {
-  extends: [
-    '@commitlint/config-conventional',
-    '@commitlint/config-pnpm-scopes',
-  ],
+  extends: ['@commitlint/config-conventional'],
 };
 
 export default config;
