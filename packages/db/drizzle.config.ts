4cc85fc012dcb35ad6e1aa45250a84bcebf90107
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-05-11 16:27:08 +0100

    chore: updates dependencies

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,10 +3,10 @@ import type { Config } from 'drizzle-kit';
 import { env } from '@orbitkit/env/web';
 
 export default {
+  dialect: 'postgresql',
   schema: './src/schema/index.ts',
   out: './drizzle',
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    url: env.DATABASE_URL,
   },
-  driver: 'pg',
 } satisfies Config;
