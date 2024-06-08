5b6987648b3b02d46466a0068c6dcdd82792df00
Corfitz <52371939+imCorfitz@users.noreply.github.com>
2024-06-08 11:45:01 +0200

    feat: adding rename-packages script (#67)
    * feat: adding rename-packages script
    
    * fix: removing unnecessary double hyphens
    
    * feat: create dedicated script utils
    
    * refactor: improved organisation and reuseability of scripts
    
    * fix: fixing comments in scripts
    
    * feat: adding exclude option
    
    * feat: adding includeroot option to scripts
    
    * refactor: changing include-root flag
    
    * feat: adding set:license script
    
    * docs(docs): updating documentation for set scripts
    
    * docs(docs): updating documentation for set scripts
    
    * refactor: updating scripts after linting
    
    * chore: updating versions and added changesets
    
    * Update set-author.ts
    
    * docs: adding jsdoc comments to custom scripts
    
    * chore: updating comment
    
    * feat: adding callback script and dependency updates in set:namespace
    
    * feat: adding rename functionality
    
    * feat: updating to promises
    
    * feat: adding bun types
    
    * refactor: removing type-fest in favour of pkg-types
    
    * feat: running bun scripts after namespace update
    
    * feat: merging scripts to one
    
    * chore: update docs & activate ci pipeline from me
    
    ---------
    
    Co-authored-by: Ahmed Elsakaan <ahmedtarek3214@gmail.com>

---
+++ .changeset/fuzzy-crabs-hear.md
@@ -0,0 +1,7 @@
+---
+'@orbitkit/docs': patch
+---
+
+docs: updated installation documentation
+
+- introduced new commands setting project metadata
--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,6 +1,4 @@
-/* eslint-disable security/detect-non-literal-fs-filename */
-import * as fs from 'fs';
-import * as path from 'path';
+import { getWorkspacePackageNames } from './scripts/utils';
 
 // ------------------------------------------------------------------
 
@@ -13,16 +11,8 @@ import * as path from 'path';
 //
 // This was previously achieved using @commitlint/config-pnpm-scopes
 // which provides a custom commitlint configuration.
-//
-// As Orbitkit has moved away from pnpm workspaces, we need to
-// implement a custom commitlint configuration to achieve the same.
 
 // ------------------------------------------------------------------
-
-interface PackageJson {
-  name: string;
-  workspaces?: string[];
-}
 
 interface Context {
   cwd?: string;
@@ -31,79 +21,9 @@ interface Context {
 // ------------------------------------------------------------------
 
 /**
- * Function to read and parse the root package.json
- * @param cwd the current working directory
- * @returns the root package.json in the workspace.
- */
-function getRootPackageJson(cwd: string): PackageJson {
-  const rootPackageJsonPath = path.resolve(cwd, 'package.json');
-  const rootPackageJson = JSON.parse(
-    fs.readFileSync(rootPackageJsonPath, 'utf-8'),
-  ) as PackageJson;
-  return rootPackageJson;
-}
-
-/**
- * Function to get all workspace package paths.
- * @param workspaces the workspaces defined in the root package.json.
- * @returns an array of all workspace package paths.
- */
-function getWorkspacePackagePaths(workspaces: string[]): string[] {
-  const workspacePackagePaths: string[] = [];
-
-  workspaces.forEach((workspacePattern) => {
-    const workspaceDirs = workspacePattern.replace(/\/\*$/, '');
-    const absolutePath = path.resolve(process.cwd(), workspaceDirs);
-    const packages = fs
-      .readdirSync(absolutePath)
-      .map((pkgDir) => path.join(workspaceDirs, pkgDir))
-      .filter((pkgPath) => fs.existsSync(path.join(pkgPath, 'package.json'))); // Filter only directories with package.json
-    workspacePackagePaths.push(...packages);
-  });
-
-  return workspacePackagePaths;
-}
-
-/**
- * Function to get package names from package.json files
- * @param packagePaths an array of package paths
- * @returns an array of package names
- */
-function getPackageNamesFromPaths(packagePaths: string[]): string[] {
-  const packageNames = packagePaths.map((pkgPath) => {
-    const packageJsonPath = path.join(pkgPath, 'package.json');
-    const packageJson = JSON.parse(
-      fs.readFileSync(packageJsonPath, 'utf-8'),
-    ) as PackageJson;
-    return packageJson.name;
-  });
-
-  return packageNames;
-}
-
-/**
- * Function to get all workspace package names
- * @param cwd the current working directory
- * @returns an array of all workspace package names
- */
-function getWorkspacePackageNames(cwd: string): string[] {
-  const rootPackageJson = getRootPackageJson(cwd);
-  if (!rootPackageJson.workspaces) {
-    throw new Error('No workspaces defined in the root package.json');
-  }
-
-  const workspacePackagePaths = getWorkspacePackagePaths(
-    rootPackageJson.workspaces,
-  );
-  const packageNames = getPackageNamesFromPaths(workspacePackagePaths);
-
-  return [...packageNames, rootPackageJson.name];
-}
-
-/**
  * Function to get all projects in the workspace
- * @param context the context object
- * @returns an array of all projects in the workspace
+ * @param context The context object
+ * @returns An array of all projects in the workspace
  */
 function getProjects(context?: Context): string[] {
   const ctx = context ?? {};
@@ -129,4 +49,3 @@ export default {
     'scope-enum': (ctx: Context) => [2, 'always', getProjects(ctx)],
   },
 };
-/* eslint-enable security/detect-non-literal-fs-filename */
