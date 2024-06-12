0bc1e1b711f9bc48a418862f011a17a6c16cec00
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-06-12 21:27:20 +0100

    refactor(ui): simplifies the ui tsup config (#80)
    * refactor(ui): simplifies the ui tsup config
    
    * fix: adds pkg-types to root pkg.json
    
    * chore: updates deps

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,41 +1,22 @@
-import { readPackageJSON } from 'pkg-types';
 import { defineConfig } from 'tsup';
 
-import {
-  formatAndWriteWithPrettier,
-  listDirectories,
-} from '@orbitkit/utils/filesystem';
-
-const getPrimitives = async () => {
-  const listOfPrimitives = (await listDirectories('./src/primitives')).map(
-    (d) => d.name,
-  );
-
-  return listOfPrimitives.map((primitive) => ({
-    source: `./src/primitives/${primitive}/index.tsx`,
-    export: `./${primitive}`,
-  }));
-};
+import { listDirectories } from '@orbitkit/utils/filesystem';
 
 /**
  * This function generates a list of entries for the build.
  * @returns List of entries for tsup.
  */
-async function getEntries() {
-  const primitives = await getPrimitives();
+function getEntries() {
+  const primitives = listDirectories('./src/primitives').map(
+    (d) => `./src/primitives/${d.name}/index.tsx`,
+  );
 
-  return [
-    ...primitives,
-    {
-      source: './src/utils/cn.ts',
-      export: './cn',
-    },
-  ];
+  return [...primitives, './src/utils/cn.ts'];
 }
 
-export default defineConfig(async (opts) => ({
-  entry: (await getEntries()).map((entry) => entry.source),
-  format: ['esm'],
+export default defineConfig((opts) => ({
+  entry: getEntries(),
+  format: ['esm', 'cjs'],
   splitting: true,
   sourcemap: true,
   minify: !opts.watch,
@@ -43,34 +24,4 @@ export default defineConfig(async (opts) => ({
   // We need to use a different tsconfig for the build because we need to exclude the story files from being compiled into the .d.ts bundle file.
   tsconfig: './tsconfig.build.json',
   outDir: 'dist',
-  async onSuccess() {
-    const pkg = await readPackageJSON();
-
-    pkg.exports = {};
-
-    const entries = await getEntries();
-
-    entries.forEach((entry) => {
-      if (typeof pkg.exports === 'object') {
-        pkg.exports = {
-          ...pkg.exports,
-          [entry.export]: {
-            import: {
-              types: entry.source
-                .replace('src', 'dist')
-                .replace(/\.tsx?$/, '.d.ts'),
-              default: entry.source
-                .replace('src', 'dist')
-                .replace(/\.tsx?$/, '.js'),
-            },
-          },
-        } as Record<string, string | Record<string, string>>;
-      }
-    });
-
-    await formatAndWriteWithPrettier({
-      content: JSON.stringify(pkg, null, 2),
-      filePath: './package.json',
-    });
-  },
 }));
