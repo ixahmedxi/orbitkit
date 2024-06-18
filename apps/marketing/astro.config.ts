ed3b60576bed661e81d460f593ccadf0e5dd3a06
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-06-18 17:13:47 +0300

    feat(marketing): sentry integration

--- apps/marketing/astro.config.ts
+++ apps/marketing/astro.config.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config'
+
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import sentry from '@sentry/astro'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
@@ -12,5 +15,13 @@ export default defineConfig({
     sitemap(),
     tailwind({ applyBaseStyles: false }),
     react(),
+    sentry({
+      dsn: import.meta.env['PUBLIC_SENTRY_DSN'] as string,
+      sourceMapsUploadOptions: {
+        org: import.meta.env['SENTRY_ORG'] as string,
+        project: import.meta.env['SENTRY_PROJECT'] as string,
+        authToken: import.meta.env['SENTRY_AUTH_TOKEN'] as string,
+      },
+    }),
   ],
 })
